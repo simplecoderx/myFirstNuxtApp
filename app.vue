dd7fc0ba22fa57f77b39daa300e354328f89340f
Lyn Vil <91258211+simplecoderx@users.noreply.github.com>
2024-02-08 12:59:55 +0800

    Created Pages

--- app.vue
+++ app.vue
@@ -1,5 +1,15 @@
 <template>
   <div>
-    Welcome to the World Nuxt 3
+    <NuxtLayout >
+      <header>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/events">Events</NuxtLink>
+          </li>
+        </ul>
+      </header>
+      <NuxtPage />
+    </NuxtLayout>
   </div>
 </template>

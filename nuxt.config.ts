9863a51d61c39f7055097bbbc21dd08c210d7164
Lyn Vil <91258211+simplecoderx@users.noreply.github.com>
2024-02-17 17:18:48 +0800

    Added hooks: nuxt config ts and myPlugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -10,6 +10,9 @@ export default defineNuxtConfig({
       }] 
     }
   },
+  hooks: {
+    ready: (ctx) => console.log(ctx),
+  },
   alias: {
    /*  "@": resolve(__dirname, "/"), */
    assets: "/<rootDir>/assets",
--- plugins/myPlugin.ts
+++ plugins/myPlugin.ts
@@ -1,10 +1,10 @@
-export default defineNuxtPlugin((nuxtApp) => {
+/* export default defineNuxtPlugin((nuxtApp) => {
     return {
         provide : {
             sayHello: (msg:string) => console.log(`Hello ${msg}`),
         },
     }
-  })
+  }) */
 /* export default defineNuxtPlugin(nuxtApp => {
     console.log(nuxtApp);
   }) */
@@ -13,3 +13,9 @@ export default defineNuxtPlugin((nuxtApp) => {
     console.log(nuxtApp);
   })
    */
+
+export default defineNuxtPlugin((nuxtApp) => {
+  nuxtApp.hook("app:created", () => {
+    console.log("hello from plugin");
+  });
+});

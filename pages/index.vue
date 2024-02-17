359432391fad3db23f376a9b0d0bf3427c692e04
Lyn Vil <91258211+simplecoderx@users.noreply.github.com>
2024-02-17 16:45:06 +0800

    Used the function useAsyncData, used refreshNuxtData and a button

--- pages/index.vue
+++ pages/index.vue
@@ -24,15 +24,22 @@ console.log(response);
 
 <script setup>
 /* const { data: products } = await useFetch('/api/products'); */
-const { data: products, pending } = await useLazyFetch('/api/products', {
+/* const { data: products, pending } = await useLazyFetch('/api/products', {
         transform: (_products) => _products.data,
-});
+}); */
 /* console.log(toRaw(products.value)); */
+
+const { data: productCount, pending } = await useAsyncData("products", () => $fetch("api/products"));
+const refresh = () => refreshNuxtData("products");
 </script>
 
 <template>
 <!-- <div>{{ products }}</div> -->
-<div>{{ pending ? "Loading" : products }}</div>
+<div>
+        <p>{{ pending ? "Loading" : productCount }}</p>
+        <button @click="refresh">Refresh</button>
+</div>
+
 </template>
 
 <style lang="scss"></style>
--- server/api/products.ts
+++ server/api/products.ts
@@ -8,11 +8,35 @@ import data from "./products.json";
 
  */
 
-
-export default defineEventHandler(async () =>{
+//useFetch and useLazyFetch
+/* export default defineEventHandler(async () =>{
     return new Promise<any>((resolve) => {
 	setTimeout(() => {
         resolve (data);
     }, 2000);
     });
+}); */
+
+let productCount = 0;
+
+/* export default async () => {
+  await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(productCount++);
+    }, 2000);
+  });
+  return {
+    productCount,
+  };
+};
+ */
+
+setInterval(() => {
+  productCount++;
+}, 1000);
+
+export default defineEventHandler(() => {
+  return {
+    productCount,
+  };
 });
